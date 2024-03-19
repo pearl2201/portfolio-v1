da4961c709a2dfaabb7e86d1d387f9cf99047f3b
nguyenanhngoc <ngoc.nguyenanh@inwave.vn>
2024-03-19 18:04:48 +0700

    no message

--- portfolio/layouts/default.vue
+++ portfolio/layouts/default.vue
@@ -1,28 +1,32 @@
 <template>
     <v-app>
         <v-main>
-            <v-container fluid class="page-wrapper pb-sm-15 pb-10">
-                <div class='maxWidth'>
-                    <div class="d-flex">
+            <v-container class="page-wrapper pb-sm-15 pb-10">
+                <div class='maxWidth d-flex flex-column'>
+                    <div class="d-flex mb-4">
                         <div class="text-h4">Pearl2201</div>
                         <v-list class="ml-auto d-flex">
-                            <v-list-item>ABOUT</v-list-item>
-                            <v-list-item>EXPERIENCE</v-list-item>
-                            <v-list-item>SKILLS</v-list-item>
-                            <v-list-item>EDUCATION</v-list-item>
-                            <v-list-item>PROJECTS</v-list-item>
+                            <v-list-item to="/">ABOUT</v-list-item>
+                            <v-list-item to="/experience">EXPERIENCE</v-list-item>
+                            <v-list-item to="/skills">SKILLS</v-list-item>
+                            <v-list-item to="/education">EDUCATION</v-list-item>
+                            <v-list-item to="/projects">PROJECTS</v-list-item>
                         </v-list>
                     </div>
-                    <NuxtPage />
+
 
                 </div>
+
+                <NuxtPage />
+
+
             </v-container>
 
         </v-main>
         <v-footer class="bg-indigo-lighten-1 text-center d-flex flex-column" :height="100">
-            <- <div>
+            <div>
                 {{ new Date().getFullYear() }} — <strong>Pearl2201</strong>
-                </div>
+            </div>
         </v-footer>
     </v-app>
 </template>
@@ -30,5 +34,7 @@
 <style scoped>
 .page-wrapper {
     min-height: calc(100vh - 100px);
+    display: flex;
+    flex-direction: column;
 }
 </style>
